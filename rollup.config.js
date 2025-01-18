953fa6bb8a08aa25865d421d35c402af59ba2896
Ryoya Tamura <lollipop@simochee.net>
2025-01-18 22:35:52 +0900

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { defineConfig } from "rollup";
+
+export default defineConfig({
+	input: "src/index.js",
+	output: {
+		file: process.env.npm_package_main,
+		format: "umd",
+	},
+	plugins: [nodeResolve(), commonjs()],
+});
